17c1ae344f33a9fa6ea95663ab31cab429ea121e
Abhishek-1337 <abhishekrinku4@gmail.com>
2026-05-17 16:10:42 +0530

    embedding the chunks

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+from collections import Counter
+
 class RAGPipeline: 
 
     def chunking(self, document, chunk_size = 250, overlap = 50):
@@ -9,9 +12,41 @@ class RAGPipeline:
             allChunks.append(" ".join(words[start:end]))
             start = end
         return allChunks
-      
 
+    def create_vocab(self, all_chunks):
+        vocab = set()
+        for chunk in all_chunks: 
+            vocab.update(chunk.lower().split())
+
+        return vocab
+
+    def compute_tf(self, text, vocab):
+        words = text.lower().split()
+        word_count = Counter(text)
+        total = len(text)
+        return [word_count.get(word, 0) / total  for word in words]
+
+    def compute_idf(self, all_chunks, vocab):
+        length = len(all_chunks)
+        idf = []
+        for word in vocab:
+            doc_count = sum(1 for chunk in all_chunks if word in chunk.lower().split())
+            idf.append(math.log((doc_count + 1)/(length + 1)) + 1)
+        return idf
+    
+    def tf_idf(self, text, vocab, idf):
+        tf = self.compute_tf(text, vocab)
+        return [i*j for i, j in zip(tf, idf)]
+
+
+        
+
+
+
+      
 
 rag = RAGPipeline()
 chunks = rag.chunking("A document is a document, you can't say a sentence is a document", 2, 1)
-print(chunks)
+
+vocab = rag.create_vocab(chunks)
+print(vocab)
